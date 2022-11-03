b4719efaa655420f50aed6358c498760315ea6b0
Matias Karmanto <matias.karmanto@tuni.fi>
2022-11-03 16:44:16 +0200

    Helloworld.js: Moved hello loop inside of loopHello-function

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,10 +1,11 @@
 //This is going to be Hello World -printer!
 
-var hello = "Hello World";
-for (let i = 0; i < 10; i++) {
-  console.log(hello);
-}
-
 async function delayedHello() {
   await setTimeout(() => console.log(hello), 1000);
+}
+function loopHello() {
+  var hello = "Hello World";
+  for (let i = 0; i < 10; i++) {
+    console.log(hello);
+  }
 }

b9248db647c588110dd8566296bd7552a9dde228
Terhi Jarvinen <terhi.jarvinen@tuni.fi>
2022-11-03 19:48:45 +0200

    HelloWorld: import java util scanner

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,7 @@
  * @author Niko Lähteinen
  * @author Terhi Järvinen
  */
-
+import java.util.Scanner;
 class HelloWorld{
 	public static void Main(String[] args){
       System.out.println("Hello World!");

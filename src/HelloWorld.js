504d91458ef319180a647d15cc7fff61be429c8b
Matias Karmanto <matias.karmanto@tuni.fi>
2022-11-04 13:10:55 +0200

    HelloWorld.js: Adds commenting to functions loopHello() and delayedHello()

---
+++ src/HelloWorld.js
@@ -0,0 +1,11 @@
+//Waits 1sec, returns promise-object with hello
+async function delayedHello() {
+  await setTimeout(() => console.log(hello), 1000);
+}
+//Prints out Hello world, 5 times with a loop
+function loopHello() {
+  var hello = "Hello World";
+  for (let i = 0; i < 5; i++) {
+    console.log(hello);
+  }
+}

760e8377a49683129386f75728aac80833e1b1e9
Terhi Jarvinen <terhi.jarvinen@tuni.fi>
2022-11-05 23:17:24 +0200

    HelloWorld: Add a random integer.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -8,6 +8,7 @@ import java.util.Random;
 class HelloWorld{
 	public static void Main(String[] args){
 	int i = 5;
+	int r = Math.random();
 	for(int j = 0; j < 5; i++){
       	 System.out.println("Hello World!");
 	}

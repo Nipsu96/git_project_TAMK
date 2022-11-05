9fb9d7137e6c43b3191089223ac961d8b780ae50
Terhi Jarvinen <terhi.jarvinen@tuni.fi>
2022-11-05 23:14:33 +0200

    HelloWorld: Add ´for loop.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -8,6 +8,8 @@ import java.util.Random;
 class HelloWorld{
 	public static void Main(String[] args){
 	int i = 5;
-      System.out.println("Hello World!");
+	for(int j = 0; j < 5; i++){
+      	 System.out.println("Hello World!");
+	}
     }
 }

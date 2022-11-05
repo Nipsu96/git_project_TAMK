c96369b50c6e4acb8043321a4d2ed047b9f2a7a2
Terhi Jarvinen <terhi.jarvinen@tuni.fi>
2022-11-05 23:12:56 +0200

    HelloWorld: Change integer value..

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 import java.util.Random;
 class HelloWorld{
 	public static void Main(String[] args){
-	int i = 0;
+	int i = 5;
       System.out.println("Hello World!");
     }
 }

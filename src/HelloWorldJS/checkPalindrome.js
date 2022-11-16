ddd66f2409d1acd147ee70af4b961bde7fe6fed6
Matias Karmanto <matias.karmanto@tuni.fi>
2022-11-16 18:22:40 +0200

    checkPalindrome.js: Adds string check functions.
    Adds string check functions, which first split the string into array,
    reverses the array and join the array back together.
    
    returns boolean value for "this" === reversed string

--- src/HelloWorldJS/checkPalindrome.js
+++ src/HelloWorldJS/checkPalindrome.js
@@ -1,6 +1,6 @@
 String.prototype.checkPalindrome() = function () {
     string = this.toLowerCase().replace(/[^a-z]+/g, "");
-
-}
+    return this === string.split("").reverse().join("");
+};
 
 console.log("saippuakauppias".checkPalindrome());

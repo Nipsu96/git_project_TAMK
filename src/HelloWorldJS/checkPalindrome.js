19034589619744cc9b873ed45a4e7167efe0a29e
Matias Karmanto <matias.karmanto@tuni.fi>
2022-11-17 11:32:23 +0200

    checkPalindrome.js: Adds comment to describe .toLowerCase() & .replace().

--- src/HelloWorldJS/checkPalindrome.js
+++ src/HelloWorldJS/checkPalindrome.js
@@ -2,6 +2,7 @@
 //Can be used as <string>.checkPalindrome to check if the word is a palindrome.
 //Returns true / false.
 String.prototype.checkPalindrome = function () {
+  //Converts the string into lowercase and removes any chars outside of alphabet.
   string = this.toLowerCase().replace(/[^a-z]+/g, "");
   return string === string.split("").reverse().join("");
 };

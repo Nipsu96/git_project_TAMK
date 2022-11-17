06fc976c10dd2218063b592e53ce6489fcaa73d1
Matias Karmanto <matias.karmanto@tuni.fi>
2022-11-17 11:29:59 +0200

    checkPalindrome.js: Adds description comment for the function

--- src/HelloWorldJS/checkPalindrome.js
+++ src/HelloWorldJS/checkPalindrome.js
@@ -1,3 +1,6 @@
+//Adds checkPalindrome function to String.prototype.
+//Can be used as <string>.checkPalindrome to check if the word is a palindrome.
+//Returns true / false.
 String.prototype.checkPalindrome = function () {
   string = this.toLowerCase().replace(/[^a-z]+/g, "");
   return string === string.split("").reverse().join("");

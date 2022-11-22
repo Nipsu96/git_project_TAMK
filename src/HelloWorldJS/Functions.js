87bfe467b76369fc5c5a15c9e1891a62294f4535
Matias Karmanto <matias.karmanto@tuni.fi>
2022-11-22 18:02:36 +0200

    Functions.js: Removes variables add5 & add10. Adds return value for anon function.

--- src/HelloWorldJS/Functions.js
+++ src/HelloWorldJS/Functions.js
@@ -1,8 +1,8 @@
 //function that returns function to add 2 numbers
 function addition(num) {
-  return function (num2) {};
+  return function (num2) {
+    return num + num2;
+  };
 }
-let add5 = addition(5);
-let add10 = addition(10);
 //Call function which adds the 2 numbers
-console.log(add10(10));
+console.log(addition(10)(10));

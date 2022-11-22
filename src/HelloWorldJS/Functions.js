b4277fedf8ef8152eaeefe7e65697369a50d11d8
Matias Karmanto <matias.karmanto@tuni.fi>
2022-11-22 18:06:20 +0200

    Functions.js: Adds comment for describing the functions.

--- src/HelloWorldJS/Functions.js
+++ src/HelloWorldJS/Functions.js
@@ -1,8 +1,9 @@
-//function that returns function to add 2 numbers
+//Function that returns function to add 2 numbers.
 function addition(num) {
+  //Returns function and the second argument is taken as parameter for this function.
   return function (num2) {
     return num + num2;
   };
 }
 //Call function which adds the 2 numbers
-console.log(addition(10)(10));
+console.log(addition(5)(10));

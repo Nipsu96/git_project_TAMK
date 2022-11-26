3f69f130ba5798a5456d044efdc7607aafa68ccc
Matias Karmanto <matias.karmanto@tuni.fi>
2022-11-26 14:20:37 +0200

    Adds Person & Sailor constructors. Sailor inherits from Person.

--- src/HelloWorldJS/InheritanceClass.js
+++ src/HelloWorldJS/InheritanceClass.js
@@ -3,4 +3,14 @@ class Person {
   constructor(name) {
     this.name = name;
   }
+  singSong() {
+    console.log(this.name + " starts singing");
+  }
+}
+class Sailor extends Person {
+  constructor(name, sailorName) {
+    this.name = super.name;
+    this.sailorName = sailorName;
+  }
+  sailing() {}
 }

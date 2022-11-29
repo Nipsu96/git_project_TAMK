83f67b9e46b2426a1cb9f30f4119ed767d5ad879
Matias Karmanto <matias.karmanto@tuni.fi>
2022-11-29 13:20:18 +0200

    super.js: Rephrases comment on Square class.

--- src/HelloWorldJS/super.js
+++ src/HelloWorldJS/super.js
@@ -4,7 +4,7 @@ class Rectangle {
     return "I have 4 sides";
   }
 }
-//Child object which calls the static method with super.
+//Child object which calls the static method of the parent with super.
 class Square extends Rectangle {
   static getDescription() {
     return super.getDescription() + " which are all equal";

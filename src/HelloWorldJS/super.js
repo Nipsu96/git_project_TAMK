e98698ea143ce7ac568b9c67218dd4277f6c3e96
Matias Karmanto <matias.karmanto@tuni.fi>
2022-11-29 13:17:40 +0200

    super.js: Removes unneeded constructors from classes.

--- src/HelloWorldJS/super.js
+++ src/HelloWorldJS/super.js
@@ -1,14 +1,10 @@
 class Rectangle {
-  constructor() {}
   static getDescription() {
     return "I have 4 sides";
   }
 }
 
 class Square extends Rectangle {
-  constructor() {
-    super();
-  }
   static getDescription() {
     return super.getDescription() + " which are all equal";
   }

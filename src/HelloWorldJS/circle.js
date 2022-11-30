b7f24ca28df00192d98039fb3acc7adbe07833c3
Matias Karmanto <matias.karmanto@tuni.fi>
2022-11-30 12:37:08 +0200

    circle.js: Unifies commenting style and fixes grammar errors.

--- src/HelloWorldJS/circle.js
+++ src/HelloWorldJS/circle.js
@@ -13,29 +13,34 @@ class Circle {
     }
   }
   static max(array) {
-    // Sort to order values from lowest to highest
-    // Arrow func compares each value in array against each other
-    // If return value is negative, a comes before b
-    // If return value is positive, b comes befor a
+    // Sort to order values from lowest to highest.
+    // Arrow func compares each value in array against each other.
+    // If return value is negative, a comes before b.
+    // If return value is positive, b comes before a.
     console.log(array);
 
     array.sort((a, b) => b.radius - a.radius);
 
-    // Return highest element in array
+    // Return highest element in array.
     return array[0];
   }
 }
-// sets radius for all circles
+
+// Sets radius for all circles
 let a = new Circle(25);
 let b = new Circle(40);
 let c = new Circle(50);
 let d = new Circle(80);
-// changes radius, uses setter
+
+// Changes radius, uses setter
 c.radius = 30;
-// returns radius, uses getter
+
+// Returns radius, uses getter
 console.log(c.radius);
-//stores largest Circle to variable
+
+// Stores largest Circle to variable
 let largest = Circle.max([a, b, c, d]);
-//print the biggest radius
+
+// Print the biggest radius
 console.log(largest);
-//c.radius = -70; // crashes app, radius must be > 0
+// c.radius = -70; // crashes app, radius must be > 0
